22f5aa7ccff3c357353043fe70e062f91273ce97
jameshsu1125 <jameshsu1125@gmail.com>
2026-01-21 10:18:03 +0800

    fix: update URL construction for uploads and listing in BunnyCDN integration
    - Adjusted URL generation logic to handle folder names correctly during upload and listing.
    - Updated package version to 0.0.2.
    - Refactored Vite configuration for improved readability and added named exports in Rollup options.

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,js}"],
+    files: ['**/*.{ts,js}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     rules: {},
-  }
+  },
 );
--- vite.config.js
+++ vite.config.js
@@ -5,49 +5,50 @@ import dtsPlugin from 'vite-plugin-dts';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
-    root: resolve(__dirname, 'src'),
-    publicDir: resolve(__dirname, 'public'),
-    build: {
-        emptyOutDir: true,
-        outDir: '../lib',
-        lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
-            name: 'index',
-            fileName: 'index',
-            formats: ['es', 'cjs'],
+  base: './',
+  root: resolve(__dirname, 'src'),
+  publicDir: resolve(__dirname, 'public'),
+  build: {
+    emptyOutDir: true,
+    outDir: '../lib',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: { output: { exports: 'named' } },
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        math: 'always',
+        globalVars: {
+          mainColor: 'red',
         },
+      },
     },
-    css: {
-        preprocessorOptions: {
-            less: {
-                math: 'always',
-                globalVars: {
-                    mainColor: 'red',
-                },
-            },
-        },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
     },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src'),
-        },
-    },
-    assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
-    plugins: [
-        cssInjectedByJsPlugin(),
-        dtsPlugin({ insertTypesEntry: true, outDir: '../lib' }),
-        nodePolyfills({
-            globals: {
-                Buffer: true,
-                global: true,
-                process: true,
-            },
-            protocolImports: true,
-        }),
-    ],
-    server: {
-        open: true,
-        port: 5173,
-    },
+  },
+  assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
+  plugins: [
+    cssInjectedByJsPlugin(),
+    dtsPlugin({ insertTypesEntry: true, outDir: '../lib' }),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    }),
+  ],
+  server: {
+    open: true,
+    port: 5173,
+  },
 });
